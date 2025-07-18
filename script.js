22591fff88e18949231306398982bfb277bb43c2
pierce-g <piercejgorman@gmail.com>
2025-07-18 22:44:57 +0100

    Data is read from weather api and displayed on page.

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ const api_url = "http://api.weatherapi.com/v1/current.json";
 const inputField = document.getElementById("location");
 const button = document.getElementById("searchButton");
 
+const weatherImage = document.getElementById("image")
 const temperatureText = document.getElementById("temperature");
 const weatherText = document.getElementById("weather");
 
 button.addEventListener("click", sendRequest)
 
 function sendRequest(){
-    fetch("http://api.weatherapi.com/v1/current.json?key=64393e073c1248deb84210224251807&q=London&aqi=no")
-        .then(response => response.json())
-        .then(data => console.log(data))
+    fetch(api_url+"?key="+api_key+"&q="+inputField.value+"&aqi=no")
+        .then(response => {
+
+            if(!response.ok){
+                throw new Error("Could not find location");
+            }
+            return response.json();
+        })
+        .then(data => {
+            temperatureText.innerHTML = data.current.temp_c;
+            weatherText.innerHTML = data.current.condition.text;
+        })
         .catch(error => console.error(error))
 }
